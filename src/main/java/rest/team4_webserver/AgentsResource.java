95ce2f117fc74c65f9722d1772fd1c8ed9fd285d
Brett Dawson <dawson.bt@gmail.com>
2022-04-27 23:08:04 -0600

    While attempting to add a set of GET, POST, etc. methods for another entity class (Agent) I managed to break the whole thing. More work to do tomorrow...

--- src/main/java/rest/team4_webserver/AgentsResource.java
+++ src/main/java/rest/team4_webserver/AgentsResource.java
@@ -1,106 +1,109 @@
 package rest.team4_webserver;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import model.Package;
+import model.Agent;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.lang.reflect.Type;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 
-@Path("/package")
-public class PackagesResource {
-    public void PackageResource() {
+@Path("/agent")
+public class AgentsResource {
+    public void AgentResource() {
         try {
             DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+    // experimenting with a void method and Persistence context annotation. might fix the problem I'm having
+@PersistenceContext
+EntityManager em;
+    public void findAgent(int agtId) {
+        Agent agent = em.find(Agent.class, agtId);
+        agent.getId();
+    }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("getpackages")
-    public String getPackages() {
+    @Path("getagents")
+    public String getAgents() {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        Query query = entityManager.createQuery("select p from Package p"); // could also use find() method
-//        List<Package> list = query.getResultList();
+//        Query query = entityManager.createQuery("select a from Agent a");
+//        List<Agent> list = query.getResultList();
 //        Gson gson = new Gson();
-//        Type type = new TypeToken<List<Package>>() {
-//        }.getType(); // create TypeToken object, TypeToken needs a forced subclass (adding the curly braces), then calling an object from getType method
-
-        return entityManager.toString(); // PLACEHOLDER, NEEDS TO BE FIXED
+//        Type type = new TypeToken<List<Agent>>(){}.getType();
+//        return gson.toJson(list, type);
+        return entityManager.toString(); // PLACEHOLDER - NEEDS TO BE FIXED
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("getpackage/{pkgId}")
-    public String getPackage(@PathParam("pkgId") int pkgId) {
+    @Path("getagent/{agtId}")
+    public String getAgent(@PathParam("agtId") int agtId) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Package pkg = entityManager.find(Package.class, pkgId);
+        Agent agt = entityManager.find(Agent.class, agtId);
         Gson gson = new Gson();
-        Type type = new TypeToken<List<Package>>() {
-        }.getType();
-        return gson.toJson(pkg, type);
+//        Type type = new TypeToken<List<Agent>>() {
+//        }.getType();
+        return gson.toJson(agt);
     }
 
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("postpackage")
-    public String postPackage(String jsonString) {
+    @Path("postagent")
+    public String postAgent(String jsonString) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         Gson gson = new Gson();
-        Package packageObject = gson.fromJson(jsonString, Package.class);
+        Agent agentObject = gson.fromJson(jsonString, Agent.class);
         entityManager.getTransaction().begin();
-        Package mergedObject = entityManager.merge(packageObject);
+        Agent mergedObject = entityManager.merge(agentObject);
         entityManager.getTransaction().commit();
         entityManager.close();
-        return "{'message': 'Package was updated'}";
+        return "{'message': 'Agent was updated'}";
     }
 
     @PUT
-    @Consumes({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON}) // MAKE SURE YOU ADD CURLY BRACES WITHIN THIS ANNOTATION
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("insertpackage")
-    public String putPackage(String jsonString) {
+    @Path("insertagent")
+    public String putAgent(String jsonString) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         Gson gson = new Gson();
-        Package packageObject = gson.fromJson(jsonString, Package.class);
+        Agent agentObject = gson.fromJson(jsonString, Agent.class);
         entityManager.getTransaction().begin();
-        entityManager.persist(packageObject);
+        entityManager.persist(agentObject);
         entityManager.getTransaction().commit();
         entityManager.close();
-        return "{'message': 'Package was inserted'}";
+        return "{'message': 'New agent was inserted'}";
     }
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("deletepackage/{pkgId}")
-    public String deletePackage(@PathParam("pkgId") int pkgId) {
+    @Path("deleteagent/{agtId}")
+    public String deleteAgent(@PathParam("agtId") int agtId) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Package pkg = entityManager.find(Package.class, pkgId);
+        Agent agt = entityManager.find(Agent.class, agtId);
         String message;
-        if (pkg == null) {
-            message = "{'message': 'Package not found'}";
+        if (agt == null) {
+            message = "{'message': 'Agent not found'}";
         } else {
             entityManager.getTransaction().begin();
-            entityManager.remove(pkg);
+            entityManager.remove(agt);
             entityManager.getTransaction().commit();
-            message = "{'message': 'Package was deleted'}";
+            message = "{'message': 'Agent was deleted'}";
         }
         return message;
     }
